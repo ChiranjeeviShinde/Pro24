f898af213f438d2ba00d01aa825c1495e2a69d51
ChiranjeeviShinde <74903894+ChiranjeeviShinde@users.noreply.github.com>
2021-01-01 16:59:30 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,16 @@
+class bin{
+    constructor(x,y,width,height){
+        var options_bin = {
+            isStatic:true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options_bin);
+        this.width = width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+    display(){
+     var pos = this.body.position
+     fill("purple");
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,43 @@
+
+var bin1,bin2,bin3;
+var paperBall;
+var groundObject;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function setup() {
+	createCanvas(1600, 700);
+   
+    engine = Engine.create();
+	world = engine.world;
+
+    bin1 = new bin(1165,650,200,20);
+    bin2 = new bin(1055,610,20,100);
+	bin3 = new bin(1260,610,20,100);
+	paperBall = new paper(110,445,30);
+	groundObject=new ground(width/2,670,width,20);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+   bin1.display();
+   bin2.display();
+   bin3.display();
+   paperBall.display();
+   groundObject.display();
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paperBall.body,paperBall.body.position,{x:50,y:-50});
+	 }
+   }
